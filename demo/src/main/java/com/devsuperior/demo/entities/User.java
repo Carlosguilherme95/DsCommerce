4d7199a861e941f5b7b08bd031a26852dfeba01e
Carlos <carlos_buske@hotmail.com>
2025-12-30 18:43:30 -0300

    criando  a classe Payment

--- demo/src/main/java/com/devsuperior/demo/entities/User.java
+++ demo/src/main/java/com/devsuperior/demo/entities/User.java
@@ -51,6 +51,54 @@ public class User {
         return Objects.hash(id, name, email, phone, birthDate, password);
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public List<Order> getOrders() {
         return orders;
     }
