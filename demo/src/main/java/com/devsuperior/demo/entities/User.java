82d85b86feab5c056fd0ad71a1343732666cde43
Carlos <carlos_buske@hotmail.com>
2025-12-31 15:21:09 -0300

    equals hashcode

--- demo/src/main/java/com/devsuperior/demo/entities/User.java
+++ demo/src/main/java/com/devsuperior/demo/entities/User.java
@@ -44,18 +44,6 @@ public class User {
 
     public User() {};
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(phone, user.phone) && Objects.equals(birthDate, user.birthDate) && Objects.equals(password, user.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, email, phone, birthDate, password);
-    }
-
     public Long getId() {
         return id;
     }
@@ -106,5 +94,18 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
