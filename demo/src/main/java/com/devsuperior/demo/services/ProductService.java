362b9e3d5de53cc39029e6d729801dc8c13df926
Carlos <carlos_buske@hotmail.com>
2026-01-05 20:04:59 -0300

    get com paginação

--- demo/src/main/java/com/devsuperior/demo/services/ProductService.java
+++ demo/src/main/java/com/devsuperior/demo/services/ProductService.java
@@ -4,9 +4,12 @@ import com.devsuperior.demo.dto.ProductDTO;
 import com.devsuperior.demo.entities.Product;
 import com.devsuperior.demo.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,4 +27,9 @@ public class ProductService {
         return dto;
     }
 
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = productRepository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+    }
 }
