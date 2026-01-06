bdd4d0ab4bc0908a8dd938b2345a6ce07279c528
Carlos <carlos_buske@hotmail.com>
2026-01-05 22:53:53 -0300

    bean validation

--- demo/src/main/java/com/devsuperior/demo/dto/ValidationError.java
+++ demo/src/main/java/com/devsuperior/demo/dto/ValidationError.java
@@ -1,4 +1,21 @@
 package com.devsuperior.demo.dto;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ValidationError {
+public class ValidationError extends CustomError {
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError(Instant timestamp, Integer status, String error, String path) {
+        super(timestamp, status, error, path);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldsName,String message){
+        errors.add(new FieldMessage(fieldsName,message));
+    }
 }
